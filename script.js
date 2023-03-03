f90307b3e1c7ae99be6b50780ded0e3e02931cfc
Leonid <leonideko1@gmail.com>
2023-03-03 14:20:01 +0200

    js radag

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-document.getElementById("main-action").onclick = function () {
-    document.getElementById("cars").scrollIntoView({ behavior: "smooth" });
-  };
-  var buttons = document.getElementsByClassName("car-button");
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function () {
-      document.getElementById("price").scrollIntoView({ behavior: "smooth" });
-    };
-  }
-  
-  document.getElementById("price-action").onclick = function () {
-    if (document.getElementById("name").value === "") {
-      alert("Заповніть поле ім'я!");
-    } else if (document.getElementById("phone").value === "") {
-      alert("Заповніть поле телефон!");
-    } else if (document.getElementById("car").value === "") {
-      alert("Заповніть поле автомобіль!");
-    } else {
-      alert("Дякуємо за заявку, ми зв'яжемся з вами найближчим часом!!");
-    }
-  };
-  document.addEventListener("DOMContentLoaded", function () {
-    let layer = document.querySelector(".price-image");
-    document.addEventListener("mousemove", (event) => {
-      layer.style.transform =
-        "translate3d(" +
-        (event.clientX * 0.4) / 7 +
-        "px," +
-        (event.clientY * 0.3) / 8 +
-        "px,0px)";
-    });
-  
-    const elem = document.querySelector(".main");
-    document.addEventListener("scroll", () => {
-      elem.style.backgroundPositionX = "0" + 0.3 * window.pageYOffset + "px";
-    });
+const mainAction = document.getElementById("main-action");
+mainAction.addEventListener("click", () => {
+  const cars = document.getElementById("cars");
+  cars.scrollIntoView({ behavior: "smooth" });
+});
+
+const buttons = document.querySelectorAll(".car-button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const price = document.getElementById("price");
+    price.scrollIntoView({ behavior: "smooth" });
   });
+});
+
+const priceAction = document.getElementById("price-action");
+priceAction.addEventListener("click", () => {
+  const name = document.getElementById("name");
+  const phone = document.getElementById("phone");
+  const car = document.getElementById("car");
   
+  if (name.value === "") {
+    alert("Заповніть поле ім'я!");
+  } else if (phone.value === "") {
+    alert("Заповніть поле телефон!");
+  } else if (car.value === "") {
+    alert("Заповніть поле автомобіль!");
+  } else {
+    alert("Дякуємо за заявку, ми зв'яжемося з вами найближчим часом!!");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const layer = document.querySelector(".price-image");
+  document.addEventListener("mousemove", (event) => {
+    const translateX = (event.clientX * 0.4) / 7;
+    const translateY = (event.clientY * 0.3) / 8;
+    layer.style.transform = `translate3d(${translateX}px, ${translateY}px, 0px)`;
+  });
+
+  const elem = document.querySelector(".main");
+  document.addEventListener("scroll", () => {
+    const bgPositionX = 0.3 * window.pageYOffset;
+    elem.style.backgroundPositionX = `0${bgPositionX}px`;
+  });
+});
+
+
+
